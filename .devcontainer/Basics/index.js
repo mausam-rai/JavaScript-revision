7720c9c290d60bd4b0b1fd66ab9f8378275f55c4
mausam-rai <123714660+mausam-rai@users.noreply.github.com>
2023-12-06 14:23:27 +0000

    container config

--- .devcontainer/Basics/index.js
+++ .devcontainer/Basics/index.js
@@ -14,3 +14,21 @@ console.log("Array first", arry);
 let arry2= arry.splice(1,3);
 console.log(`Splice of an Array: ${arry2}`);
 console.log("Array Second", arry);
+
+
+
+let one=[1,2,3,4,5];
+let two=[6,7,8];
+let three=[...one,...two];
+// spread operator ...
+console.log(three);
+
+let score=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score,score2,score3));
+
+console.log(Array.from("Mausam"));
+let ars=[1,2,[3,4],[6,7,[8]]];
+console.log(ars.flat(Infinity));
