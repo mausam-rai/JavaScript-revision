ed229af3b597d38d81eb8bd88c3dd0ff9ef90ca9
mausam-rai <123714660+mausam-rai@users.noreply.github.com>
2023-12-06 13:48:36 +0000

    conatiner config

--- .devcontainer/Basics/index.js
+++ .devcontainer/Basics/index.js
@@ -1,1 +1,16 @@
 console.log("hello");
+let balance= 400;
+let cash = new Number(400);
+console.log(typeof balance);
+console.log( cash);
+
+
+let arry=[1,2,3,4,5];
+let newArry=arry.slice(1,3);
+console.log(`Slice of array : ${newArry}`);
+console.log("Array first", arry);
+
+// Here in splice , manipulates the original array 
+let arry2= arry.splice(1,3);
+console.log(`Splice of an Array: ${arry2}`);
+console.log("Array Second", arry);
